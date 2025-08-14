37a723bcd325d024430623e4e4bf65b0fc00a950
nguyennv96 <nguyen.js.bb96@gmail.com>
2025-08-15 00:17:13 +0700

    Cấu hình router

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import { store } from '@/store/store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { router } from '@/routers/index'
 
-createApp(App).use(store).use(ElementPlus).mount('#app')
+createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
---
+++ src/routers/index.js
@@ -0,0 +1,58 @@
+import { createWebHistory, createRouter } from 'vue-router'
+
+import LoginView from '@/views/login.vue'
+import App from '@/App.vue'
+import Signup from '@/views/signup.vue'
+import Dashboard from '@/views/dashboard.vue'
+import VerifyEmail from '@/views/verify-email.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: App,
+    redirect: '/login',
+    children: [
+      {
+        name: 'Login',
+        path: 'login',
+        component: LoginView,
+      },
+      {
+        name: 'Signup',
+        path: 'signup',
+        component: Signup,
+      },
+      {
+        name: 'Verify',
+        path: 'account/verification',
+        component: VerifyEmail,
+      },
+      {
+        name: 'Dashboard',
+        path: 'dashboard',
+        component: Dashboard,
+      },
+    ],
+  },
+]
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+router.beforeEach((to, from, next) => {
+  const whiteList = ['Login', 'Signup', 'Verify']
+  const isAuthenticated = false
+
+  if (!whiteList.includes(to.name) && !isAuthenticated) {
+    next({ name: 'Login' })
+  } else if (isAuthenticated && whiteList.includes(to.name)) {
+    if (from.name) {
+      next({ name: from.name })
+    } else {
+      next({ name: 'Dashboard' })
+    }
+  } else {
+    next()
+  }
+})
