2b160b1d563b4bb064e7d7cb5900425896f74def
nguyennv96 <nguyen.js.bb96@gmail.com>
2025-08-17 02:58:46 +0700

    Tạo màn và component  list board

---
+++ src/components/board-tile.vue
@@ -0,0 +1,16 @@
+<script setup>
+import bgBoardDefault from '@/assets/images/bg-board-default.jpg'
+const props = defineProps(['backgroundImg', 'title'])
+</script>
+<template>
+  <div class="h-[150px] border-gray-700 shadow-sm rounded-bl-lg rounded-br-lg">
+    <div
+      class="p-4 shadow bg-cover bg-center h-[70%] rounded-tl-lg rounded-tr-lg"
+      :style="{ backgroundImage: `url(${props.backgroundImg || bgBoardDefault})` }"
+    ></div>
+    <h3 class="text-blue-900 font-medium text-sm p-2">
+      {{ props.title }}
+    </h3>
+  </div>
+</template>
+<style scoped></style>
---
+++ src/views/board.vue
@@ -0,0 +1,10 @@
+<script setup>
+import BoardTile from '@/components/board-tile.vue'
+</script>
+<template>
+  <h1 class="mb-5 font-semibold">Danh sách bảng của bạn</h1>
+  <div class="grid grid-cols-5 gap-5">
+    <BoardTile v-for="index in 10" v-bind:key="index" :background-img="null" title="Test board" />
+  </div>
+</template>
+<style scoped></style>
